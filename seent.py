8febe355724b1e60d91e683ccbf21f9af75d91f1
Nick Kiermaier <nickkiermaier@gmail.com>
2015-11-23 17:45:36 -0600

    Update seent.py

--- seent.py
+++ seent.py
@@ -1,6 +1,5 @@
 # Volatility facebook plugin
 #
-# Copyright (C) 2013 Jeff Bryner (jeff@jeffbryner.com)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
