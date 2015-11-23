8eea8af779d0d7ac1f411494df5d1de031bf7946
Nick Kiermaier <nickkiermaier@gmail.com>
2015-11-23 17:37:15 -0600

    remove old strings

--- seent.py
+++ seent.py
@@ -16,12 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 #
-"""
-@author:       Jeff Bryner (p0wnlabs)
-@license:      GNU General Public License 2.0 or later
-@contact:      jeff@jeffbryner.com
-@organization: p0wnlabs.com
-"""
+
 
 import volatility.timefmt as timefmt
 import volatility.obj as obj
